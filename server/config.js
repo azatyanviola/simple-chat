24e1f8dbe217688e2042fbcdb3cca35e783c646e
azatyan viola <viola.azatyan@mail.ru>
2022-07-13 22:39:03 -0700

    Deploy my login and chat application

---
+++ server/config.js
@@ -0,0 +1,27 @@
+
+function ExtractJwt(req) {
+    let token = null;
+    if (req.cookies && req.cookies.token != void 0) {
+      token = req.cookies["token"];
+    }
+    return token;
+  }
+  //.process.env.Monhodb_uri
+ 
+  module.exports = {
+    jwt: {
+      jwtFromRequest: ExtractJwt,
+      secretOrKey: "TfbTq2NfLzqMcbVY9EpGQ2p"
+    },
+  
+    expiresIn: "1 day",
+}
+//     mongo: {
+//       url: 'mongodb+srv://Avioleta:q2LFzHOnql0hSd7M@node-chat.lmbptbi.mongodb.net/?retryWrites=true&w=majority' || "mongodb://localhost:27017/",
+//       options: {
+//         dbName: "chatik",
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//       }
+//     }
+//   };
---
+++ server/server.js
@@ -0,0 +1,61 @@
+
+global.Promise = require("bluebird");
+const env = require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+
+
+const express = require("express");
+const app = express();
+const nunjucks = require("nunjucks");
+const server = require("http").Server(app);
+const io = require("socket.io")(server, { serveClient: true });
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const config = require("./config");
+
+const passport = require("passport");
+const { Strategy } = require("passport-jwt");
+
+const { jwt } = require("./config");
+
+passport.use(
+  new Strategy(jwt, function(jwt_payload, done) {
+    if (jwt_payload != void 0) {
+      return done(false, jwt_payload);
+    }
+    done();
+  })
+);
+
+// mongoose.connect(config.mongo.url, config.mongo.options);
+// mongoose.set("debug", process.env.NODE_ENV !== "production");
+// mongoose.connection.on("error", e => {
+//   console.error("MongoDB connection error", e);
+//   process.exit(0);
+// });
+
+const url = 'mongodb+srv://Avioleta:q2LFzHOnql0hSd7M@node-chat.lmbptbi.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(url, {useNewUrlParser: true});
+
+nunjucks.configure("./client/views", {
+  autoescape: true,
+  express: app
+});
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+// parse application/json
+app.use(express.json());
+
+app.use(cookieParser());
+
+require("./router")(app);
+
+require("./sockets")(io);
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server started on port ${PORT}`);
+});
